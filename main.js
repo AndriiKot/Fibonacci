46d8e52cb8fe4eaec6299ca73ae2fc5ddc8e37fa
AndriiKot <a_kocyuba@ukr.net>
2023-03-22 18:35:51 +0100

    Ruby methods imitation Js function

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 ﻿function myFunction1(a,b) {
  return a * b
 }
-
 console.log(myFunction1(3,5))   // 15
+
+// or
+
+function myFunction2(a,b) {return a / b}
+console.log(8,4)               //  2
+
+
+function myFunction3(a,b) {return a + b}
+console.log(myFunction3())        // NaN
+
+
+let x = function(a,b){return a - b}
+let y = x(15,5)                   
+console.log(y)                    // 10
--- main.rb
+++ main.rb
@@ -1,3 +1,27 @@
-﻿def myMethod1(a,b) = a * b
+﻿def myMethod1 a,b
+  a * b
+end
+p myMethod1(3,5)               # 15
 
-p myMethod1(3,5)             # 15
+
+
+def myMethod2(a,b) = a * b
+p myMethod2(8,4)               # 2
+
+def myMethod3(a,b) = a + b
+#p myMethod3()                 wrong number of arguments(given 0 , exended 2)
+
+def myMethod4(a = nil,b = a) = (return a + b if (a && b); 0 / 0.0)
+p myMethod4                    # NaN     imitation js function   !!!
+
+def myMethod5(a = nil,b = a) = proc{ |a,b| a - b if (a && b)}   # imitation js function
+x = myMethod5                 # Proc
+z = x[15,5]
+p z                           # 10
+
+
+z = x                         # Proc
+p z[]                         # nil
+p z[23,16]                    # 7
+                   
+
