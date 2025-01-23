39fbd7ba6beacd5b9726d8243d97ed3a5087f684
AndriiKot <AndriiKot@users.noreply.github.com>
2025-01-23 18:18:18 +0100

    docs: create two webWorkers v-for and v-matrix-exponentiation

--- WebWorkers/getFibonacciNumberByIndex/webWorker__v-recursion-bad-practice.js
+++
@@ -1,9 +0,0 @@
-const fibGetValueNumber = (n) => {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fibGetValueNumber(n - 1) + fibGetValueNumber(n - 2);
-  }
-};
-
-export default fibGetValueNumber;
